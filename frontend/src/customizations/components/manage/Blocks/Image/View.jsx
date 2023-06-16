4e02ed67c1c75ca29d59093c7bdf82b318c0e379
Cihan Andac <cihanandac@gmail.com>
2023-06-16 13:28:18 +0200

    Fix the caption issue

--- frontend/src/customizations/components/manage/Blocks/Image/View.jsx
+++ frontend/src/customizations/components/manage/Blocks/Image/View.jsx
@@ -43,16 +43,9 @@ export const View = ({ data, detached }) => {
                   isInternalURL(data.url)
                     ? // Backwards compat in the case that the block is storing the full server URL
                       (() => {
-                        if (data.size === 'l')
-                          return `${flattenToAppURL(data.url)}/@@images/image`;
-                        if (data.size === 'm')
-                          return `${flattenToAppURL(
-                            data.url,
-                          )}/@@images/image/preview`;
-                        if (data.size === 's')
-                          return `${flattenToAppURL(
-                            data.url,
-                          )}/@@images/image/mini`;
+                        if (data.size === 'l') return `${flattenToAppURL(data.url)}/@@images/image`;
+                        if (data.size === 'm') return `${flattenToAppURL(data.url)}/@@images/image/preview`;
+                        if (data.size === 's') return `${flattenToAppURL(data.url)}/@@images/image/mini`;
                         return `${flattenToAppURL(data.url)}/@@images/image`;
                       })()
                     : data.url
@@ -71,10 +64,7 @@ export const View = ({ data, detached }) => {
                     small: data.size === 's',
                   })}`}
                 >
-                  <UniversalLink
-                    href={href}
-                    openLinkInNewTab={data.openLinkInNewTab}
-                  >
+                  <UniversalLink href={href} openLinkInNewTab={data.openLinkInNewTab}>
                     {image}
                   </UniversalLink>
                   <p id="photo-credit">
@@ -94,25 +84,10 @@ export const View = ({ data, detached }) => {
                   })}`}
                 >
                   {image}
-                  {/* {data.textalign === 'left' ? (
-                    <p id="photo-credit" className="left">
-                      <br />
-                      {data.alt} <br />
-                      {data.copyright}
-                    </p>
-                  ) : data.textalign === 'right' ? (
-                    <p id="photo-credit" className="right">
-                      <br />
-                      {data.alt} <br />
-                      {data.copyright}
-                    </p>
-                  ) : (
-                    <p id="photo-credit">
-                      <br />
-                      {data.alt} <br />
-                      {data.copyright}
-                    </p>
-                  )} */}
+                  <p id="photo-credit">
+                    {data.alt} <br />
+                    {data.copyright}
+                  </p>
                 </div>
               );
             }
