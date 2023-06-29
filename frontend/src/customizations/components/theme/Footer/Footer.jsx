e6c9bdce5ee96c9104798329449781d2a46f5390
Cihan Andac <cihanandac@gmail.com>
2023-06-29 12:53:20 +0200

    Add styling for the Tickets section of the footer

--- frontend/src/customizations/components/theme/Footer/Footer.jsx
+++ frontend/src/customizations/components/theme/Footer/Footer.jsx
@@ -48,6 +48,62 @@ const Footer = ({ intl }) => {
 
   return (
     <div id="footerWrapper">
+      <div id="Tickets">
+        <h3 className="Header">Het huis met eigen ogen bekijken?</h3>
+        <div className="buttons">
+          <button className="button button1" href="/">
+            Tickets
+          </button>
+          <button className="button button2" href="/">
+            Menu
+          </button>
+        </div>
+      </div>
+      <div id="Newsletter">
+        <h3 className="Header">Nieuws over Rietveld in je mail</h3>
+        <form
+          id="newsletter-form"
+          method="get"
+          action="https://centraalmuseum.us2.list-manage.com/subscribe/post-json?c=?"
+        >
+          <div id="formfield-form-widgets-email">
+            <input
+              id="form-widgets-email"
+              name="EMAIL"
+              className="text-widget required textline-field"
+              value=""
+              type="text"
+              placeholder="Je emailadres"
+            />
+          </div>
+
+          <input type="hidden" value="c04600e3ceefae8c502cbabec" name="u" />
+          <input type="hidden" value="42702e9770" name="id" />
+          <input type="hidden" value="1" name="group[15893][1]" />
+
+          <div className="formControls">
+            <input
+              id="form-buttons-subscribe"
+              name="form.buttons.subscribe"
+              className="submit-widget button-field context"
+              value="Inschrijven"
+              type="submit"
+            />
+          </div>
+
+          <div id="subscribe-result">
+            <p className="error-msg" style={{ display: 'none' }}>
+              Aanmelden op nieuwsbrief mislukt.
+            </p>
+
+            <p className="success-msg" style={{ display: 'none' }}>
+              Bedankt voor je aanmelding. Je ontvangt een e-mail waarin je
+              inschrijving wordt bevestigd.
+            </p>
+          </div>
+        </form>
+      </div>
+
       <div id="Footer">
         <div className="site-logo">
           <Image
