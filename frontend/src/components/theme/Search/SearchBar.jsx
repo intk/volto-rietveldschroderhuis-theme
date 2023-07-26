45cb5118048397be652900548ea1f00940b13927
Cihan Andac <cihanandac@gmail.com>
2023-07-26 16:33:34 +0200

    Add search page stylings

---
+++ frontend/src/components/theme/Search/SearchBar.jsx
@@ -0,0 +1,95 @@
+/**
+ * Search widget component.
+ * @module components/theme/SearchWidget/SearchWidget
+ */
+
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form, Input } from 'semantic-ui-react';
+import { defineMessages, useIntl } from 'react-intl';
+import { useDetectClickOutside } from 'react-detect-click-outside';
+
+const messages = defineMessages({
+  search: {
+    id: 'Search',
+    defaultMessage: 'Search',
+  },
+  searchSite: {
+    id: 'Search Site',
+    defaultMessage: 'Search Site',
+  },
+});
+
+/**
+ * SearchWidget component class.
+ * @class SearchWidget
+ * @extends Component
+ */
+const SearchBar = ({ onClose }) => {
+  const [text, setText] = React.useState('');
+  const [visible, setVisible] = React.useState(false);
+  const history = useHistory();
+  const intl = useIntl();
+  const ref = useDetectClickOutside({
+    onTriggered: () => {
+      setText('');
+      setVisible(false);
+    },
+  });
+  const inputRef = React.useRef(null);
+
+  const onChangeText = (e, { value }) => {
+    setText(value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setVisible(false);
+    history.push(`/search?SearchableText=${encodeURIComponent(text)}`);
+    // reset input value
+    setText('');
+    onClose();
+  };
+
+  return (
+    <div ref={ref} className="searchbar">
+      <Form action="/search" onSubmit={onSubmit}>
+        <Form.Field className={`searchbox${visible ? ' visible' : ''}`}>
+          <Input
+            id="input"
+            ref={inputRef}
+            aria-label={intl.formatMessage(messages.searchSite)}
+            onChange={onChangeText}
+            name="SearchableText"
+            value={text}
+            transparent
+            autoComplete="off"
+            placeholder={intl.formatMessage(messages.searchSite)}
+            title={intl.formatMessage(messages.search)}
+          />
+          <button
+            type="submit"
+            aria-label={intl.formatMessage(messages.search)}
+            onClick={(e) => {
+              if (!text?.length) {
+                e.preventDefault();
+
+                if (inputRef?.current) {
+                  inputRef.current.focus();
+                }
+
+                setVisible(true);
+              } else {
+                onSubmit();
+              }
+            }}
+          >
+            {intl.formatMessage(messages.search)}
+          </button>
+        </Form.Field>
+      </Form>
+    </div>
+  );
+};
+
+export default SearchBar;
