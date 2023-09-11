22d1379d15190f763ca454d1f21047e235b68c12
Cihan Andac <cihanandac@gmail.com>
2023-09-11 17:37:38 +0200

    fix socialmediablock

--- frontend/src/components/Blocks/SocialMediaButtons/SocialMediaButtonsView.jsx
+++ frontend/src/components/Blocks/SocialMediaButtons/SocialMediaButtonsView.jsx
@@ -2,6 +2,24 @@ import { injectIntl } from 'react-intl';
 import './css/socialmediablock.less';
 
 const SocialMediaButtonsView = (props) => {
+  const handleFacebookShareClick = (e) => {
+    e.preventDefault();
+    const shareURL =
+      'https://www.facebook.com/sharer/sharer.php?u=' +
+      encodeURIComponent(window.location.href);
+    window.open(shareURL, 'Popup', 'width=600,height=400');
+  };
+
+  const handleTwitterShareClick = (e) => {
+    e.preventDefault();
+    const shareText = 'Rietveld Schröderhuis: ';
+    const shareURL =
+      'https://twitter.com/intent/tweet?text=' +
+      encodeURIComponent(shareText) +
+      encodeURIComponent(window.location.href);
+    window.open(shareURL, 'Popup', 'width=600,height=400');
+  };
+
   return (
     <div id="social-media-block">
       <div className="text">
@@ -9,16 +27,9 @@ const SocialMediaButtonsView = (props) => {
       </div>
       <div className="buttons">
         <a
-          class="popup"
-          href="https://www.facebook.com/sharer/sharer.php?u=https://www.rietveldschroderhuis.nl/nl/ontdek/rietveld-schroderhuis"
-          onClick={(e) => {
-            e.preventDefault();
-            window.open(
-              'https://www.facebook.com/sharer/sharer.php?u=https://www.rietveldschroderhuis.nl/nl/ontdek/rietveld-schroderhuis',
-              'Popup',
-              'width=600,height=400',
-            );
-          }}
+          className="popup"
+          href="https://www.facebook.com/sharer/sharer.php?u=https://www.rietveldschroderhuis.nl"
+          onClick={handleFacebookShareClick}
         >
           <svg
             className="button"
@@ -41,16 +52,9 @@ const SocialMediaButtonsView = (props) => {
           </svg>
         </a>
         <a
-          class="popup"
-          href="https://twitter.com/intent/tweet?text=Rietveld Schröderhuis: https://www.rietveldschroderhuis.nl/nl/ontdek/rietveld-schroderhuis"
-          onClick={(e) => {
-            e.preventDefault();
-            window.open(
-              'https://twitter.com/intent/tweet?text=Rietveld Schröderhuis: https://www.rietveldschroderhuis.nl/nl/ontdek/rietveld-schroderhuis',
-              'Popup',
-              'width=600,height=400',
-            );
-          }}
+          className="popup"
+          href="https://twitter.com/intent/tweet?text=Rietveld Schröderhuis: https://www.rietveldschroderhuis.nl"
+          onClick={handleTwitterShareClick}
         >
           <svg
             className="button"
@@ -62,7 +66,12 @@ const SocialMediaButtonsView = (props) => {
             <path d="M24.253 8.756C24.69 17.08 18.297 24.182 9.97 24.62c-3.122.162-6.22-.646-8.86-2.32 2.702.18 5.375-.648 7.507-2.32-2.072-.248-3.818-1.662-4.49-3.64.802.13 1.62.077 2.4-.154-2.482-.466-4.312-2.586-4.412-5.11.688.276 1.426.408 2.168.387-2.135-1.65-2.73-4.62-1.394-6.965C5.574 7.816 9.54 9.84 13.802 10.07c-.842-2.738.694-5.64 3.434-6.48 2.018-.624 4.212.043 5.546 1.682 1.186-.213 2.318-.662 3.33-1.317-.386 1.256-1.248 2.312-2.4 2.942 1.048-.106 2.07-.394 3.02-.85-.458 1.182-1.343 2.15-2.48 2.71z" />
           </svg>
         </a>
-        <a href="mailto:info@rietveldschroderhuis.nl?subject=Rietveld%20Schr%C3%B6derhuis&body=https%3A%2F%2Fwww.rietveldschroderhuis.nl%2Fnl%2Fontdek%2Frietveld-schroderhuis">
+        <a
+          className="popup"
+          href="mailto:info@rietveldschroderhuis.nl?subject=Rietveldschroderhuiswebsite"
+          // eslint-disable-next-line react/jsx-no-target-blank
+          target="_blank"
+        >
           <svg
             className="button"
             xmlns="http://www.w3.org/2000/svg"
