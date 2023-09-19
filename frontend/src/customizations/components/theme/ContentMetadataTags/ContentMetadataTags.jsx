5055ef2eaf31752bbc6d36040891951f4d931010
Cihan Andac <cihanandac@gmail.com>
2023-09-19 17:03:45 +0200

    Fix bug for string expectation

--- frontend/src/customizations/components/theme/ContentMetadataTags/ContentMetadataTags.jsx
+++ frontend/src/customizations/components/theme/ContentMetadataTags/ContentMetadataTags.jsx
@@ -49,16 +49,17 @@ const ContentMetadataTags = (props) => {
     <>
       <Helmet>
         <title>
-          {(seo_title || title)?.replace(/\u00AD/g, '')}
-          {props?.content['@type'] !== 'LRF'
-            ? props.content?.language?.token === 'nl'
-              ? ' - Rietveld Schröderhuis'
-              : props.content?.language?.token === 'en'
-              ? ' - Rietveld Schroder House'
-              : props.content?.language?.token === 'de'
-              ? ' - Rietveld-Schroder-Haus'
-              : ' - Rietveld Schröderhuis'
-            : ''}
+          {`${(seo_title || title)?.replace(/\u00AD/g, '')}${
+            props?.content['@type'] !== 'LRF'
+              ? props.content?.language?.token === 'nl'
+                ? ' - Rietveld Schröderhuis'
+                : props.content?.language?.token === 'en'
+                ? ' - Rietveld Schroder House'
+                : props.content?.language?.token === 'de'
+                ? ' - Rietveld-Schroder-Haus'
+                : ' - Rietveld Schröderhuis'
+              : ''
+          }`}
         </title>
         <meta name="description" content={seo_description || description} />
         <meta
